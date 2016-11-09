9a43548e3fde34325d5ad5e8d7e010b6fd796a19
Brian Chan <b123400@gmail.com>
2016-11-10 01:01:52 +0800

    Implement basic functional layout!

--- src/c/1440.c
+++ src/c/1440.c
@@ -1,15 +1,49 @@
 #include <pebble.h>
+#include "math.h"
 
 static Window *s_window;
 static Layer *bitmap_layer;
 
+static GColor ttmm_red;
+
 static void bitmap_layer_update_proc(Layer *layer, GContext* ctx) {
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  int minute = (*t).tm_min;
+  int hour = (*t).tm_hour;
+  int tens = (int)(minute + hour * 60) / 10;
+
   GRect bounds = layer_get_bounds(layer);
-  GPoint center = grect_center_point(&bounds);
+
+  // background color
+  graphics_context_set_fill_color(ctx, ttmm_red);
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+
+  float x_margin = 0.1;
+  float y_margin = 0.1;
+  float start_x = bounds.size.w * x_margin;
+  float x_step = (bounds.size.w * (1 - x_margin * 2)) / 11;
+
+  float start_y = bounds.size.h * y_margin;
+  float y_step = (bounds.size.h * (1 - y_margin * 2)) / 11;
+
   graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_circle(ctx, center, 77);
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_circle(ctx, center, 52);
+  for (int i = 0; i < 12; i++) {
+    for (int j = 0; j < 12; j++) {
+
+      int dot_size = 3;
+      if (i * 12 + j < tens) {
+        dot_size = 7;
+      }
+
+      GRect dot = GRect(
+        (int)round(start_x + x_step * i - (dot_size-1)/2),
+        (int)round(start_y + y_step * j - (dot_size-1)/2),
+        dot_size,
+        dot_size);
+      graphics_fill_rect(ctx, dot, 0, GCornerNone);
+    }
+  }
 }
 
 static void prv_window_load(Window *window) {
@@ -25,7 +59,17 @@ static void prv_window_unload(Window *window) {
   layer_destroy(bitmap_layer);
 }
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  int minute = (*tick_time).tm_min;
+  int hour = (*tick_time).tm_hour;
+  if ((minute + hour * 60) % 10 == 0) {
+    layer_mark_dirty(bitmap_layer);
+  }
+}
+
 static void prv_init(void) {
+  ttmm_red = GColorFromRGBA(205, 34, 49, 255);
+
   s_window = window_create();
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = prv_window_load,
@@ -33,6 +77,8 @@ static void prv_init(void) {
   });
   const bool animated = true;
   window_stack_push(s_window, animated);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void prv_deinit(void) {
